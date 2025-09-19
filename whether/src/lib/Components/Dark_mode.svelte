f6c4a670f35c0f6f36d19d24f3b1dc9019d16206
Thomas Visser (1000134991) <VISSTL1@student.op.ac.nz>
2025-09-19 13:49:47 +1200

    Implemented Dark Mode

---
+++ whether/src/lib/Components/Dark_mode.svelte
@@ -0,0 +1,32 @@
+<script>
+    let mode = $state("Dark");
+    function switchMode() {
+        if (mode === "Dark")
+        {
+            mode = "Light";
+        }
+        else
+        {
+            mode = "Dark";
+        }
+        }
+</script>
+
+<button onclick={switchMode}>{mode} Mode</button>
+{#if mode === "Light"}
+<style>
+    body {
+        transition: 0.3s ease-in-out;
+        background-color: white;
+        color: #333;
+    }
+</style>
+{:else}
+<style>
+    body {
+        transition: 0.3s ease-in-out;
+        background-color: #333;
+        color: white;
+    }
+</style>
+{/if}
