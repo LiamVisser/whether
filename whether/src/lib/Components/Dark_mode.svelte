bc841788b54e6761267af1d92ed6f59e0807a2d0
Thomas Visser (1000134991) <VISSTL1@student.op.ac.nz>
2025-09-19 13:56:13 +1200

    Added button styling

--- whether/src/lib/Components/Dark_mode.svelte
+++ whether/src/lib/Components/Dark_mode.svelte
@@ -13,12 +13,34 @@
 </script>
 
 <button onclick={switchMode}>{mode} Mode</button>
+<style>
+    button {
+        transition: 0.3s ease-in-out;
+    border: none; /* Remove borders */
+    padding: 15px 32px; /* Some padding */
+    text-align: center; /* Centered text */
+    text-decoration: none; /* Remove underline */
+    font-size: 16px; /* Increase font size */
+    margin: 4px 2px; /* Add some margin */
+    cursor: pointer; /* Pointer cursor on hover */
+    border-radius: 50px; /* Rounded corners */
+    font-family: monospace;
+    float:right;
+    }
+ </style>
+
+
 {#if mode === "Light"}
 <style>
     body {
         transition: 0.3s ease-in-out;
         background-color: white;
         color: #333;
+    }
+    button {
+        transition: 0.3s ease-in-out;
+        background-color:cornflowerblue; /* Green background */
+        color: white; /* White text */
     }
 </style>
 {:else}
